ce5f497baa1d3a2d0b5818b638f14025537722ac
JonnyDeates <jonnydeates@gmail.com>
2025-02-08 00:55:35 -0600

    added money slots and images to jokers.

--- main.lua
+++ main.lua
@@ -9,16 +9,16 @@
 
 
 SMODS.Atlas{
-    key = 'RouletteWheel', --atlas key
+    key = 'CasinoMods', --atlas key
     path = 'Jokers.png', --atlas' path in (yourMod)/assets/1x or (yourMod)/assets/2x
     px = 71, --width of one card
     py = 95 -- height of one card
 }
 -- Create a helper function that builds a roulette joker card
-local function createRouletteCard(params)
+local function createRouletteCard(name,params)
     -- Base template with common properties and functions
     local base = {
-        atlas = 'RouletteWheel',
+        atlas = 'CasinoMods',
         rarity = 1,
         cost = 3,
         unlocked = true,
@@ -28,6 +28,7 @@ local function createRouletteCard(params)
         perishable_compat = true,
         pos = {x = 0, y = 0},
         loc_txt = {
+            name = name,
             text = {
                 '{C:green}#4# in #1#{} chance',
                 'to {X:mult,C:white}X#3#{} Mult.',
@@ -70,18 +71,80 @@ local function createRouletteCard(params)
     -- Register the joker card using the SMODS system
     SMODS.Joker(base)
 end
+-- Create a helper function that builds a roulette joker card
+local function createMoneySlotsCard(name, params)
+    -- Base template with common properties and functions
+    local base = {
+        atlas = 'CasinoMods',
+        rarity = 1,
+        cost = 3,
+        unlocked = true,
+        discovered = true,
+        blueprint_compat = true,
+        eternal_compat = true,
+        perishable_compat = true,
+        pos = {x = 0, y = 0},
+        loc_txt = {
+            name = name,
+            text = {
+                'Every round, spend {C:red}-$#1#{} to',
+                'have a chance at winning:',
+                '{C:green}#4# in #2#{} chance for {C:money}$#3#{}',
+                '{C:green}#7# in #2#{} chance for {C:money}$#6#{}',
+                '{C:green}#10# in #2#{} chance for {C:money}$#9#{}',
+                '{C:green}#13# in #2#{} chance for {C:money}$#12#{}',
+            }
+        },
+        loc_vars = function(self, info_queue, center)
+            return {
+                vars = {
+                    center.ability.extra.costs,         -- #1#: costs
+                    center.ability.extra.overall_odds,         -- 2#: overall_odds
+                    center.ability.extra.payout_1,         -- 3#: odds
+                    "" .. (G.GAME and G.GAME.probabilities.normal or 1),  -- #4#: player's probability
+                    center.ability.extra.odds_2,         -- #5#: odds
+                    center.ability.extra.payout_2,         -- #6#: payout
+                    "" .. (G.GAME and G.GAME.probabilities.normal * center.ability.extra.odds_2 or 1),  -- #7#: player's probability
+                    center.ability.extra.odds_3,         -- #8#: odds
+                    center.ability.extra.payout_3,              -- #9#: payout
+                    "" .. (G.GAME and G.GAME.probabilities.normal * center.ability.extra.odds_3 or 1),  -- #10#: player's probability
+                    center.ability.extra.odds_4,         -- #11#: odds
+                    center.ability.extra.payout_4,       -- #12#: payout
+                    "" .. (G.GAME and G.GAME.probabilities.normal * center.ability.extra.odds_4 or 1),  -- #13#: player's probability
+                }
+            }
+        end,
+        calculate = function(self, card, context)
+            if context.joker_main then
+                local normalProb = G.GAME.probabilities.normal * card.ability.extra.wins
+                local odds = card.ability.extra.odds
+                if math.random() < (normalProb / odds) then
+                    return {
+                        card = card,
+                        Xmult_mod = card.ability.extra.Xmult,
+                        message = 'X' .. card.ability.extra.Xmult,
+                        colour = G.C.MULT
+                    }
+                end
+            end
+        end,
+        in_pool = function(self, _, _)
+            return true
+        end,
+    }
 
+    -- Merge the custom parameters into the base table
+    for k, v in pairs(params) do
+        base[k] = v
+    end
+
+    -- Register the joker card using the SMODS system
+    SMODS.Joker(base)
+end
 -- Now define each roulette card using the helper
 
-createRouletteCard{
+createRouletteCard('Single',{
     key = 'roulette-wheel-green',
-    loc_txt = {
-        name = 'Single',
-        text = {
-            '{C:green}#4# in #1#{} chance',
-            'to {X:mult,C:white}X#3#{} Mult.',
-        }
-    },
     config = {
         extra = {
             odds = 37,
@@ -90,17 +153,10 @@ createRouletteCard{
         }
     },
     pos = {x = 1, y = 0},
-}
+})
 
-createRouletteCard{
+createRouletteCard('Split', {
     key = 'roulette-wheel-split',
-    loc_txt = {
-        name = 'Split',
-        text = {
-            '{C:green}#4# in #1#{} chance',
-            'to {X:mult,C:white}X#3#{} Mult.',
-        }
-    },
     config = {
         extra = {
             odds = 37,
@@ -109,18 +165,10 @@ createRouletteCard{
         }
     },
      pos = {x = 2, y = 0},
+})
 
-}
-
-createRouletteCard{
+createRouletteCard('Street',{
     key = 'roulette-wheel-street',
-    loc_txt = {
-        name = 'Street',
-        text = {
-            '{C:green}#4# in #1#{} chance',
-            'to {X:mult,C:white}X#3#{} Mult.',
-        }
-    },
     config = {
         extra = {
             odds = 37,
@@ -129,17 +177,10 @@ createRouletteCard{
         }
     },
     pos = {x = 3, y = 0},
-}
+})
 
-createRouletteCard{
+createRouletteCard('Corner', {
     key = 'roulette-wheel-corner',
-    loc_txt = {
-        name = 'Corner',
-        text = {
-            '{C:green}#4# in #1#{} chance',
-            'to {X:mult,C:white}X#3#{} Mult.',
-        }
-    },
     config = {
         extra = {
             odds = 37,
@@ -148,16 +189,9 @@ createRouletteCard{
         }
     },
     pos = {x = 4, y = 0},
-}
-createRouletteCard{
+})
+createRouletteCard('Double Street', {
     key = 'roulette-wheel-dbl-street',
-    loc_txt = {
-        name = 'Double Street',
-        text = {
-            '{C:green}#4# in #1#{} chance',
-            'to {X:mult,C:white}X#3#{} Mult.',
-        }
-    },
     config = {
         extra = {
             odds = 37,
@@ -166,16 +200,9 @@ createRouletteCard{
         }
     },
     pos = {x = 0, y = 1},
-}
-createRouletteCard{
+})
+createRouletteCard('Dozen',{
     key = 'roulette-wheel-dozen',
-    loc_txt = {
-        name = 'Dozen',
-        text = {
-            '{C:green}#4# in #1#{} chance',
-            'to {X:mult,C:white}X#3#{} Mult.',
-        }
-    },
     config = {
         extra = {
             odds = 37,
@@ -184,16 +211,9 @@ createRouletteCard{
         }
     },
     pos = {x = 1, y = 1},
-}
-createRouletteCard{
+})
+createRouletteCard('Red',{
     key = 'roulette-wheel-red',
-    loc_txt = {
-        name = 'Red',
-        text = {
-            '{C:green}#4# in #1#{} chance',
-            'to {X:mult,C:white}X#3#{} Mult.',
-        }
-    },
     config = {
         extra = {
             odds = 37,
@@ -202,77 +222,58 @@ createRouletteCard{
         }
     },
     pos = {x = 2, y = 1},
-}
--- The consumable type can remain as is (or be similarly refactored if more types share common behavior)
-SMODS.ConsumableType{
-    key = 'DerekConsumableType',
-    collection_rows = {4,5},
-    primary_colour = G.C.PURPLE,
-    secondary_colour = G.C.DARK_EDITION,
-    loc_txt = {
-        collection = 'Derek Cards',
-        name = 'Derek',
-        undiscovered = {
-            name = 'Hidden Derek',
-            text = {'Derek is', 'not here'}
-        }
-    },
-    shop_rate = 1,
-}
-
-
-
-SMODS.UndiscoveredSprite{
-    key = 'DerekConsumableType', --must be the same key as the consumabletype
-    atlas = 'Jokers',
-    pos = {x = 0, y = 0}
-}
-
-
-SMODS.Consumable{
-    key = 'Derek', --key
-    set = 'DerekConsumableType', --the set of the card: corresponds to a consumable type
-    atlas = 'Jokers', --atlas
-    pos = {x = 0, y = 0}, --position in atlas
-    loc_txt = {
-        name = 'Derek Consumable', --name of card
-        text = { --text of card
-            'Add Negative to up to #1# selected cards',
-            'and create a Derek consumable'
-        }
-    },
+})
+createMoneySlotsCard('Penny Slots', {
+    key = 'money-slots-penny-slots',
     config = {
         extra = {
-            cards = 5, --configurable value
+            costs = 1,
+            overall_odds = 500,
+            payout_1 = 20,
+            odds_2 = 10,
+            payout_2 = 10,
+            odds_3 = 100,
+            payout_3 = 3,
+            odds_4 = 250,
+            payout_4 = 1,
         }
     },
-    loc_vars = function(self,info_queue, center)
-        return {vars = {center.ability.extra.cards}} --displays configurable value: the #1# in the description is replaced with the configurable value
-    end,
-    can_use = function(self,card)
-        if G and G.hand then
-            if #G.hand.highlighted ~= 0 and #G.hand.highlighted <= card.ability.extra.cards then --if cards in hand highlighted are above 0 but below the configurable value then
-                return true
-            end
-        end
-        return false
-    end,
-    use = function(self,card,area,copier)
-        for i = 1, #G.hand.highlighted do
-            --for every card in hand highlighted
-
-            G.hand.highlighted[i]:set_edition({negative = true},true)
-            --set their edition to negative
-        end
-
-        local newcard = create_card('DerekConsumableType', G.consumeables) --create a derek consumable
-        newcard:add_to_deck() --add it to deck
-        G.consumeables:emplace(newcard) --place it into G.consumeables
-
-    end,
-}
-
-
-
+    pos = {x = 3, y = 1},
+})
+createMoneySlotsCard('Soothing Slots', {
+    key = 'money-slots-soothing-slots',
+    config = {
+        extra = {
+            costs = 5,
+            overall_odds = 500,
+            odds_2 = 10,
+            odds_3 = 100,
+            odds_4 = 250,
+            payout_1 = 100,
+            payout_2 = 50,
+            payout_3 = 15,
+            payout_4 = 5,
+        }
+    },
+    pos = {x = 4, y = 1},
+})
+createMoneySlotsCard('High Roller', {
+    key = 'money-slots-high-roller-slots',
+    rarity = 2,
+    config = {
+        extra = {
+            costs = 10,
+            overall_odds = 500,
+            odds_2 = 10,
+            odds_3 = 100,
+            odds_4 = 250,
+            payout_1 = 200,
+            payout_2 = 100,
+            payout_3 = 30,
+            payout_4 = 10,
+        }
+    },
+    pos = {x = 0, y = 2},
+})
 ----------------------------------------------
 ------------MOD CODE END----------------------
